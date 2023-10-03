2e715b60d021e2ac5e8df68f5e3a475238579bb7
magic-hell <magic_hell@163.com>
2023-10-03 12:01:11 +0800

    补交

---
+++ reggie_take_out/src/main/java/com/magichell/reggie/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package com.magichell.reggie.controller;
+
+import com.magichell.reggie.common.R;
+import com.magichell.reggie.entity.Category;
+import com.magichell.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("category:{}",category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+
+
+}
